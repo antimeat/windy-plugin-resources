cec127942f84e9297d696aeae4879e34bb15a0b7
antimeat <anti.meat@gmail.com>
2022-03-03 07:49:27 +0800

    fixed valaris position

--- dist/plugin.js
+++ dist/plugin.js
@@ -8,7 +8,7 @@ W.loadPlugin(
 /* Mounting options */
 {
   "name": "windy-plugin-resources",
-  "version": "1.2.3",
+  "version": "1.2.4",
   "author": "Darryl Vink",
   "repository": {
     "type": "git",
